d4d7d54f3c40644f5e3da19ff128b2493df7251e
endulum <71853916+endulum@users.noreply.github.com>
2025-01-13 09:59:24 -0800

    reformat app.ts, throw error if critical env vars are missing

--- app.ts
+++ app.ts
@@ -1,59 +1,76 @@
-import dotenv from 'dotenv';
-dotenv.config({ path: '.env' })
-dotenv.config({ path: '.env.' + process.env.ENV })
+console.warn(`In ${process.env.NODE_ENV} mode...`);
 
-import './config/passport';
-import path from 'path';
-import flash from 'connect-flash';
-import express from 'express';
-import session from 'express-session';
-import asyncHandler from 'express-async-handler'
-import passport from 'passport';
-import { PrismaSessionStore } from '@quixo3/prisma-session-store';
-import { PrismaClient } from '@prisma/client';
+import dotenv from "dotenv";
+dotenv.config({ path: ".env." + process.env.NODE_ENV });
 
-// import logSession from './src/middleware/logSession';
-import errorHandler from './src/middleware/errorHandler';
-import { router as authRouter } from './src/routes/authRouter';
-import { router as mainRouter } from './src/routes/mainRouter'
+if (
+  process.env.DATABASE_URL === undefined ||
+  process.env.DATABASE_URL.trim() === ""
+) {
+  throw new Error("Database URL is not defined.");
+}
 
-const secret: string | undefined = process.env.SECRET
-if (secret === undefined) throw new Error('Secret is not defined.')
+if (
+  process.env.SESSION_SECRET === undefined ||
+  process.env.SESSION_SECRET.trim() === ""
+) {
+  throw new Error("Session secret is not defined.");
+}
 
-const app = express()
+import "./config/passport";
+import express from "express";
+import session from "express-session";
+import asyncHandler from "express-async-handler";
+import path from "path";
+import flash from "connect-flash";
+import passport from "passport";
 
-app.set('views', path.join(__dirname, 'src/views'))
-app.set('view engine', 'ejs')
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(session({
-  secret,
-  resave: false,
-  saveUninitialized: true,
-  store: new PrismaSessionStore(
-    new PrismaClient(),
-    {
+import errorHandler from "./src/middleware/errorHandler";
+import { router as authRouter } from "./src/routes/authRouter";
+import { router as mainRouter } from "./src/routes/mainRouter";
+
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import { PrismaClient } from "@prisma/client";
+
+const app = express();
+
+app.set("views", path.join(__dirname, "src/views"));
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(new PrismaClient(), {
       checkPeriod: 2 * 60 * 1000,
       dbRecordIdIsSessionId: true,
-      dbRecordIdFunction: undefined
-    }
-  ),
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24
-  }
-}))
-app.use(flash())
-app.use(passport.initialize())
-app.use(passport.session())
+      dbRecordIdFunction: undefined,
+    }),
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use(asyncHandler(async (req, res, next) => {
-  res.locals.user = req.user
-  res.locals.warning = req.flash('warning')
-  res.locals.success = req.flash('success')
-  if (req.user) return mainRouter(req, res, next)
-  else return authRouter(req, res, next)
-}))
+app.use(
+  asyncHandler(async (req, res, next) => {
+    res.locals.user = req.user;
+    res.locals.warning = req.flash("warning");
+    res.locals.success = req.flash("success");
+    if (req.user) return mainRouter(req, res, next);
+    else return authRouter(req, res, next);
+  })
+);
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-app.listen(3000)
+const port = process.env.PORT ?? 3000;
+app.listen(port, () => {
+  console.warn(`⚡️ server starting at http://localhost:${port}`);
+});
