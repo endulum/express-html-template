8535bd825e7047c0b74d44332451159b4a231abb
endulum <71853916+endulum@users.noreply.github.com>
2025-01-13 10:14:05 -0800

    move client to `/prisma`, improve reset file, start `/queries` dir

---
+++ prisma/client.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
+
+dotenv.config({ path: `.env.${process.env.ENV}` });
+
+const client = new PrismaClient({
+  datasources: { db: { url: process.env.DATABASE_URL } },
+});
+
+client.$connect();
+
+export { client };
--- prisma/reset.ts
+++ prisma/reset.ts
@@ -1,42 +1,19 @@
-import { PrismaClient } from "@prisma/client";
-import bcrypt from 'bcryptjs'
+import dotenv from "dotenv";
+dotenv.config({ path: ".env." + process.env.NODE_ENV });
 
-import dotenv from 'dotenv'
-dotenv.config({ path: '.env.' + process.env.ENV })
+import { client } from "./client";
+import * as devQueries from "./queries/dev";
 
-const prisma = new PrismaClient({ 
-  log: ['query'],
-  datasources: { db: { url: process.env.DATABASE_URL } }
-});
-
-async function clearDatabase() {
-  await prisma.user.deleteMany()
-  await prisma.session.deleteMany()
+async function reset() {
+  await devQueries.truncateTable("User");
+  await devQueries.truncateTable("Session");
+  await devQueries.createAdmin();
 }
 
-async function generateAdmin() {
-  if (!process.env.ADMINPASS) return;
-  const salt = await bcrypt.genSalt(10)
-  const hashedPassword = await bcrypt.hash(process.env.ADMINPASS, salt)
-  await prisma.user.create({
-    data: {
-      username: 'admin',
-      password: hashedPassword,
-      id: 1,
-      role: 'ADMIN'
-    }
-  })
-}
-
-async function main() {
-  await clearDatabase()
-  await generateAdmin()
-}
-
-main()
-  .catch(e => {
+reset()
+  .catch((e) => {
     console.error(e.message);
   })
   .finally(async () => {
-    await prisma.$disconnect();
+    await client.$disconnect();
   });
