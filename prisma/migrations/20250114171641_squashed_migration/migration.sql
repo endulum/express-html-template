81ceee66f7a4ddb50bd46a4e962575a93707030b
endulum <71853916+endulum@users.noreply.github.com>
2025-01-14 09:21:21 -0800

    squash

--- prisma/migrations/20250114171641_squashed_migration/migration.sql
+++ prisma/migrations/20250114171641_squashed_migration/migration.sql
@@ -3,25 +3,28 @@ CREATE TYPE "Role" AS ENUM ('BASIC', 'ADMIN');
 
 -- CreateTable
 CREATE TABLE "Session" (
-    "sid" VARCHAR NOT NULL,
-    "sess" JSON NOT NULL,
-    "expire" TIMESTAMP(6) NOT NULL,
+    "id" TEXT NOT NULL,
+    "sid" TEXT NOT NULL,
+    "data" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Session_pkey" PRIMARY KEY ("sid")
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "username" VARCHAR(32) NOT NULL,
-    "password" VARCHAR(255) NOT NULL,
     "role" "Role" NOT NULL DEFAULT 'BASIC',
+    "password" VARCHAR(255),
+    "githubUser" TEXT,
+    "githubId" INTEGER,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE INDEX "IDX_session_expire" ON "Session"("expire");
+CREATE UNIQUE INDEX "Session_sid_key" ON "Session"("sid");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
